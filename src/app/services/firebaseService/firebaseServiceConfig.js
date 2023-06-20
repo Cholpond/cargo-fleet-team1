a37bf08992060943fa745b1513910e5f79dc0afe
cholpond <dobulbekovach@gmail.com>
2023-06-20 15:50:48 -0700

    updated the firebase configurations

--- src/app/services/firebaseService/firebaseServiceConfig.js
+++ src/app/services/firebaseService/firebaseServiceConfig.js
@@ -1,22 +1,36 @@
 const prodConfig = {
-  apiKey: 'AIzaSyAo9XhYAvVgtVynXridFdsg4Qtb0DvgTo0',
-  authDomain: 'emplosoft-2db9f.firebaseapp.com',
-  projectId: 'emplosoft-2db9f',
-  databaseURL: 'https://emplosoft-2db9f-default-rtdb.firebaseio.com',
-  storageBucket: 'emplosoft-2db9f.appspot.com',
-  messagingSenderId: '460039525265',
-  appId: '1:460039525265:web:aec32080d87de88acf0030'
+  apiKey: "AIzaSyCTfmXdSTIDciaux7w1IvVdrRvDqkg_Yug",
+  authDomain: "cargo-fleet-test.firebaseapp.com",
+  databaseURL: "https://cargo-fleet-test-default-rtdb.firebaseio.com",
+  projectId: "cargo-fleet-test",
+  storageBucket: "cargo-fleet-test.appspot.com",
+  messagingSenderId: "316762921556",
+  appId: "1:316762921556:web:6deeb63c4b0f45669e6bc1"
 };
+
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+
+// Initialize Firebase
+
+
 const devConfig = {
-  apiKey: 'AIzaSyAo9XhYAvVgtVynXridFdsg4Qtb0DvgTo0',
-  authDomain: 'emplosoft-2db9f.firebaseapp.com',
-  projectId: 'emplosoft-2db9f',
-  databaseURL: 'https://emplosoft-2db9f-default-rtdb.firebaseio.com',
-  storageBucket: 'emplosoft-2db9f.appspot.com',
-  messagingSenderId: '460039525265',
-  appId: '1:460039525265:web:aec32080d87de88acf0030'
+  apiKey: "AIzaSyCTfmXdSTIDciaux7w1IvVdrRvDqkg_Yug",
+  authDomain: "cargo-fleet-test.firebaseapp.com",
+  databaseURL: "https://cargo-fleet-test-default-rtdb.firebaseio.com",
+  projectId: "cargo-fleet-test",
+  storageBucket: "cargo-fleet-test.appspot.com",
+  messagingSenderId: "316762921556",
+  appId: "1:316762921556:web:6deeb63c4b0f45669e6bc1"
 };
 
 const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 export default config;
