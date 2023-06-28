28a77e0bda9a18432f64c87802a40ae04a50637f
aida2603 <aida.aitenova@yahoo.com>
2023-06-28 15:25:14 -0400

    added forgot-password and reset pasword files

---
+++ src/app/main/apps/reset-password/ResetPasswordPageConfig.js
@@ -0,0 +1,35 @@
+import { authRoles } from 'app/auth';
+import ResetPasswordPage from './ResetPasswordPage';
+
+const ResetPasswordPageConfig = {
+  settings: {
+    layout: {
+      config: {
+        navbar: {
+          display: false
+        },
+        toolbar: {
+          display: false
+        },
+        footer: {
+          display: false
+        },
+        leftSidePanel: {
+          display: false
+        },
+        rightSidePanel: {
+          display: false
+        }
+      }
+    }
+  },
+  auth: authRoles.onlyGuest,
+  routes: [
+    {
+      path: '/reset-password',
+      component: ResetPasswordPage
+    }
+  ]
+};
+
+export default ResetPasswordPageConfig;
